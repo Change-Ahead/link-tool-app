049da421c9ba51d467081b90d9c14677c65ce8e4
Matt Barnfield <matt.barnfield@softwire.com>
2022-04-28 11:50:18 +0100

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
 
-function App() {
-    return (
-        <div className="">
-        </div>
-    );
-}
+const App: React.FC = () => {
+    return <Router>
+        <Routes>
+            <Route path="/employment" element={<div>employement</div>} />
+            <Route path="/well-being" element={<div>well being</div>}/>
+            <Route path="/essentials" element={<div>essentials</div>}/>
+            <Route path="/housing" element={<div>housing</div>}/>
+            <Route path="/" element={<div>home</div>} />
+        </Routes>
+    </Router>;
+};
 
 export default App;
