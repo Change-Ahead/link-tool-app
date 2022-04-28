c327046c37b6739c615768cb0431d4cc26f875bb
oskarwilliams378 <47484139+oskarwilliams378@users.noreply.github.com>
2022-04-28 16:25:04 +0100

    feat: Update constants (#17)

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
 import {Question} from "./types/Question";
 
 export const questions: Question[] = [
-    {id: "housing", name: "Housing", link: "/housing", checkboxes: ["Social", "Private"]},
-    {id: "well-being", name: "Well Being", link: "/well-being", checkboxes: ["Mental Health", "Physical Health"]},
+    {id: "housing", name: "Housing", link: "/housing", checkboxes: ["Social", "Private", "Shelter"]},
+    {id: "well-being", name: "Well Being", link: "/well-being", checkboxes: ["Mental Health", "Physical Health", "Spiritual", "Social", "Learning", "Giving"]},
     {id: "employment", name: "Employment", link: "/employment", checkboxes: ["Part Time", "Full Time", "Apprentices", "Volunteering"]},
-    {id: "essentials", name: "Essentials", link: "/essentials", checkboxes: ["Clothing", "Food", "Household Stuff"]},
+    {id: "essentials", name: "Essentials", link: "/essentials", checkboxes: ["Clothing", "Food", "Household Stuff", "Personal Identification Documents", "Personal Hygiene", "Tool Kits", "Electronics", "Appliances"]},
 ];
 
 export const cloudflareWorkerBaseUrl = "https://api-hook-production.tech9082.workers.dev";
