434386bc6d05e203d815798d64e3e910ca9eef6d
Oskar Williams <oskar.williams@softwire.com>
2022-04-28 17:01:31 +0100

    feat: Add logo

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -5,7 +5,8 @@ const Page: React.FC<{children: React.ReactNode}> = ({children}) => (
     <>
         <header className="w-full bg-stone-500 p-4 text-white mb-4">
             <Link to="/">
-                <h1>Link</h1>
+                <img src="/src/img/linkAheadLogo.png" className="h-12 object-contain"/>
+                <h1>Link Ahead</h1>
             </Link>
         </header>
         <main className="w-full flex justify-center">
